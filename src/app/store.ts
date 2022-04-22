4b84a71bfbd7ebedd5934820ea67f74efad058ee
Fpsska <fpsska1337@gmail.com>
2022-04-22 19:41:12 +0300

    refactor: migrate to TypeScript for store.js, index.js, App.js

--- src/app/store.ts
+++ src/app/store.ts
@@ -12,3 +12,4 @@ export const store = configureStore({
   },
 });
 
+export type RootState = ReturnType<typeof store.getState>;
--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
@@ -18,7 +18,7 @@ root.render(
         <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want your app to work offline and load faster, you can change
