d3a0038a85d7c19161e18d8446d38d3cc9a57fb4
Fpsska <fpsska1337@gmail.com>
2022-05-04 20:44:01 +0300

    feat: create Preloader component & create form-page__wrapper, form-page__form, form-page__preloader, HTML-structure in FormPage component

---
+++ src/components/common/Preloader/Preloader.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './preloader.scss';
+
+const Preloader: React.FC = () => {
+    return (
+        <div className="lds-spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Preloader;
+
--- src/components/pages/formPage/FormPage.tsx
+++ src/components/pages/formPage/FormPage.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 import Form from '../../form/Form';
+import Preloader from '../../common/Preloader/Preloader';
 
 const FormPage: React.FC = () => {
     return (
         <div className="form-page">
-            <Form />
+            <div className="form-page__wrapper">
+                <div className="form-page__form">
+                    <Form />
+                </div>
+                <div className="form-page__preloader">
+                    <Preloader />
+                </div>
+            </div>
         </div>
     );
 };
